b0cf1eb611f68c966d8434194c19cc6409e6526d
winter_x64 <itswinterx64@gmail.com>
2024-09-25 22:08:39 +0530

    Update file

--- api/main.py
+++ api/main.py
@@ -4,12 +4,19 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.route(path="/")
-def home():
-    """
-    Home route
-    """
-    return {"Project": "Something"}
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get(path="/")
+async def get_projects_all():
+    return {
+        "data": [
+            {"name": "Project 1", "creator": "winter"},
+            {"name": "Project 2", "creator": "winter"},
+        ]
+    }
 
 
 if __name__ == "__main__":
